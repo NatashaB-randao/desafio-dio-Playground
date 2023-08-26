766bde79bba14dda155dcaa7ecd0b5fe77517408
NatashaB-randao <natashabrandao57@hotmail.com>
2023-08-26 14:29:59 -0300

    Atualização da resolução

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,20 @@
 import UIKit
 
-let name: String = "Steve"
+
+let firstName: String = "Steve"
+var lastName: String?
+lastName = "Jobs"
+
+func verifyName(_ firstName: String, _ lastName: String?) {
+    print("O nome é \(firstName) \(lastName ?? "Wozniak")")
+}
+
+verifyName(firstName, lastName)
+
+
+
+
+/*let name: String = "Steve"
 var possibleLastName = "Jobs"
 var convertedPossibleLastName = String(possibleLastName)
 var otherlastname: String = "Wozniak"
@@ -9,4 +23,4 @@ if convertedPossibleLastName != nil {
     print("O nome é \(name) \(convertedPossibleLastName)")
 }else {
     print("O nome é \(name) \(otherlastname)")
-}
+}*/
